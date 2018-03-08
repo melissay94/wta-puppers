58774fbf8323edc63475a9effdb3af701ae4570b
Melissa Young <melissay94@yahoo.com>
2018-03-08 12:08:10 -0500

    Trying to fix deploy

---
+++ server.prod.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const path = require('path');
+const compression = require('compression');
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+const server = app.listen(port, () => {
+    console.log(`Working on port ${port}`);
+});
+
+app.use('/', express.static('dist'));
+
+app.use('/*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'dist', index.html));
+})
